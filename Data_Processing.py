eb3aba69c6e7d58aef61839d02394729274fd202
mikirath <sabyasachi.igit@gmail.com>
2022-03-27 00:03:53 +0530

    added sum and division function

--- Data_Processing.py
+++ Data_Processing.py
@@ -1,1 +1,11 @@
 print("Adding all the Data Processing Code related to Fraud Detection")
+
+def sum(a,b)
+    c = a+b
+    return c
+
+def multiply(a,b)
+    c = a*b
+    return c
+
+print("Implemented Sum and multiplication functions")
